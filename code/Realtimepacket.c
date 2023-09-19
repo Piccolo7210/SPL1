77bed654d10c6d69656068a3923b583b593ae3b6
Piccolo7210 <mustakim.mohsin7210@gmail.com>
2023-09-19 18:54:00 +0600

    checked

--- code/Realtimepacket.c
+++ code/Realtimepacket.c
@@ -32,7 +32,7 @@ int main(){
 		printf("Error on creating FILE.\n");
 		return -5;
 	}
-	rawSocket = socket(AF_INET, SOCK_RAW, IPPROTO_UDP);
+	rawSocket = socket(AF_INET, SOCK_RAW, IPPROTO_TCP);
 	if(rawSocket<0){
 		printf("Socket creating ERROR.\n");
 		return -2;// -2 defining as socket error
